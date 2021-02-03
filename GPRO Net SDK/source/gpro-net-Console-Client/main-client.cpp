02b8a4522d423e3d97490002b0e0285fe8f46f91
Dianna <dianna.cornett@mymail.champlain.edu>
2021-02-03 14:19:20 -0500

    adding in tutorial code through code sample 2.

--- GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
+++ GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
@@ -22,21 +22,121 @@
 	Main source for console client application.
 */
 
+
 #include "gpro-net/gpro-net.h"
 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+
 
 
 #include "RakNet/RakPeerInterface.h"
+#include "RakNet/RakNetTypes.h"
+#include "RakNet/MessageIdentifiers.h"
+
+
+//definitions for max clients and server port as shown in the tutorial
+#define MAX_CLIENTS 10
+#define SERVER_PORT 7777
 
 
 int main(int const argc, char const* const argv[])
 {
+	//sample code from RakNet (http://www.jenkinssoftware.com/raknet/manual/tutorial.html) input and debugged/ fixed for C++11 by Dianna
+	char str[512];
+	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
+	bool isServer;
+	RakNet::Packet* packet;
+
+	printf("(C) or (S)erver?\n");
+	std::cin.getline(str, 512); //tutorial uses "gets", which is deprecated and also unsafe
+	if ((str[0] == 'c') || str[0] == 'C')
+	{
+		RakNet::SocketDescriptor sd; //checked RakNet manual to see why tutorial code was throwing errors
+		peer->Startup(1, &sd, 1);
+		isServer = false;
+
+	}
+	else
+	{
+		RakNet::SocketDescriptor sd(SERVER_PORT, 0);
+		peer->Startup(MAX_CLIENTS, &sd, 1);
+		isServer = true;
+	}
+	
+	//processing inputs to determine whether to run as server or client
+	if (isServer)
+	{
+		printf("Starting the server.\n");
+		// tutorial says: this line lets the server acce[t incoming connections from the clients
+		peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+	}
+	else 
+	{
+		printf("Enter server IP or hit enter for 127.0.0.1\n ");
+		std::cin.getline(str, 512);
+		if (str[0] == 0)
+		{
+			strcpy(str, "127.0.0.1");
+		}
+		printf("Starting the client.\n");
+		peer->Connect(str, SERVER_PORT, 0, 0);
+	}
+
+	//the loop
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+				printf("Our connection request has been accepted.\n");
+				break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				if (isServer) {
+					printf("A client has disconnected.\n");
+				}
+				else {
+					printf("We have been disconnected.\n");
+				}
+				break;
+			case ID_CONNECTION_LOST:
+				if (isServer) {
+					printf("A client lost the connection.\n");
+				}
+				else {
+					printf("Connection lost.\n");
+				}
+				break;
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
 
 
+			}
+		}
+	}
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
 	printf("\n\n");
 	system("pause");
 }
