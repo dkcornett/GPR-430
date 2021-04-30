d64651bcf5d283d8267fe776e15016ccc6dc08e1
Dianna <dianna.cornett@mymail.champlain.edu>
2021-04-30 18:34:41 -0400

    further work on server/client setup

--- CommunityGarden/Assets/Scripts/ClientScript.cs
+++ CommunityGarden/Assets/Scripts/ClientScript.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+
+
+//pull into separate script later.
+public class Player
+{
+    public string playerName;
+    public GameObject avatar;
+    public int connectionId;
+}
+
 
 //client game object script
 // https://www.youtube.com/watch?v=qGkkaNkq8co used for reference for beginning setup
@@ -21,14 +32,18 @@ public class ClientScript : MonoBehaviour
     private int reliableChannel;
     private int unreliableChannel;
 
+    private int ourClientId;
     private int connectionId;
 
     private float connectionTime;
     private bool isConnected = false;
-   // private bool isStarted = false;
+    private bool isStarted = false;
     private byte error;
 
     private string playerName;
+
+    public GameObject playerPrefab;
+    public List<Player> players = new List<Player>();
 
     public void Connect()
     {
@@ -82,17 +97,79 @@ public class ClientScript : MonoBehaviour
         NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
         switch (recData)
         {
-            case NetworkEventType.Nothing: break;
-            case NetworkEventType.ConnectEvent: break;
-            case NetworkEventType.DataEvent: break;
-            case NetworkEventType.DisconnectEvent: break;
-                NetworkTransport.Disconnect(hostId, connectionId, out error);
+            case NetworkEventType.DataEvent:
+                {
+                    string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                    Debug.Log("Receiving : " + msg);
+                    string[] splitData = msg.Split('|');
+                    switch (splitData[0])
+                    {
+                        case "ASKNAME":
+                            OnAskName(splitData);
+                            break;
+
+                        case "CNN":
+                            SpawnPlayer(splitData[1], int.Parse(splitData[2]));
+                            break;
+
+                        case "DC":
+                            break;
+
+                        default:
+                            Debug.Log("invalid message");
+                            break;
+                    }
+                }
+                break;
             case NetworkEventType.BroadcastEvent:
 
                 break;
         }
     }
 
+    private void OnAskName(string[] data)
+    {
+        //set this client's id
+        ourClientId = int.Parse(data[1]);
+
+        //send our name to the server
+        Send("NAME IS|" + playerName, reliableChannel);
+
+        //create all the other players
+        for (int i = 2; i < data.Length - 1; i++)
+        {
+            string[] d = data[i].Split('%');
+            SpawnPlayer(d[0], int.Parse(d[1]));
+        }
+    }
+
+    private void SpawnPlayer(string playerName, int cnnId)
+    {
+        GameObject go = Instantiate(playerPrefab) as GameObject;
+
+        // is this ours?
+        if (cnnId == ourClientId)
+        {
+            GameObject.Find("Canvas").SetActive(false);
+            isStarted = true;
+        }
+
+        Player p = new Player();
+        p.avatar = go;
+        p.playerName = playerName;
+        p.connectionId = cnnId;
+        p.avatar.GetComponentInChildren<TextMesh>().text = playerName;
+        players.Add(p);
+    }
+
+    private void Send(string message, int channelId)
+    {
+        Debug.Log("Sending : " + message);
+        byte[] msg = Encoding.Unicode.GetBytes(message);
+        NetworkTransport.Send(hostId, connectionId, channelId, msg, message.Length * sizeof(char), out error);
+        
+    }
+    /*
     private void OnApplicationQuit()
     {
         //  NetworkTransport.Disconnect(hostId, out connectionId, out reliableChannel, out recBuffer, out bufferSize, out error);
@@ -101,5 +178,5 @@ public class ClientScript : MonoBehaviour
 
         NetworkTransport.Disconnect(hostId, connectionId, out error);
 
-    }
+    }*/
 }
--- CommunityGarden/Assets/Scripts/ServerScript.cs
+++ CommunityGarden/Assets/Scripts/ServerScript.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
-using System.Text;
+
+
+public class ServerClient
+{
+    public int connectionId;
+    public string playerName;
+}
 
 //server game object script
 // https://www.youtube.com/watch?v=qGkkaNkq8co used for reference for beginning setup
@@ -23,6 +30,8 @@ public class ServerScript : MonoBehaviour
 
     private bool isStarted = false;
     private byte error;
+
+    private List<ServerClient> clients = new List<ServerClient>();
 
     private void Start()
     {
@@ -63,16 +72,34 @@ public class ServerScript : MonoBehaviour
         NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
         switch (recData)
         {
-            case NetworkEventType.Nothing: break;
             case NetworkEventType.ConnectEvent:
                 {
                     Debug.Log("Player " + connectionId + " has connected. ");
+                    OnConnection(connectionId);
                     break;
                 }
             case NetworkEventType.DataEvent:
                 {
                     string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
-                    Debug.Log(" Player " + connectionId + " has sent: " + msg);
+                    Debug.Log(" Receiving from " + connectionId + " : " + msg);
+                    string[] splitData = msg.Split('|');
+                    switch (splitData[0])
+                    {
+                        case "NAMEIS":
+                            OnNameIs(connectionId, splitData[1]);
+                            break;
+
+                        case "CNN":
+                            break;
+
+                        case "DC":
+                            break;
+
+                        default:
+                            Debug.Log("invalid message");
+                            break;
+                    }
+
                     break;
                 }
             case NetworkEventType.DisconnectEvent:
@@ -85,6 +112,55 @@ public class ServerScript : MonoBehaviour
 
                 break;
         }
+    }
+
+    private void OnConnection(int cnnId)
+    {
+        // Add player to list of players
+        ServerClient c = new ServerClient();
+        c.connectionId = cnnId;
+        c.playerName = "TEMP";
+        clients.Add(c);
+
+        // When player joins server print ID
+        //Request player name and send the name of all the other players
+        string msg = "ASKNAME|" + cnnId + "|";
+        foreach (ServerClient sc in clients)
+        {
+            msg += sc.playerName + '%' + sc.connectionId + '|';
+        }
+
+        msg = msg.Trim('|');
+
+        Send(msg, reliableChannel, cnnId);
+     
+
+
+    }
+
+    private void Send(string message, int channelId, int cnnId)
+    {
+        List<ServerClient> c = new List<ServerClient>();
+        c.Add(clients.Find(x => x.connectionId == cnnId));
+        Send(message, channelId, c);
+    }
+    //overload
+    private void Send(string message, int channelId, List<ServerClient> c)
+    {
+        Debug.Log("Sending : " + message);
+        byte[] msg = Encoding.Unicode.GetBytes(message);
+        foreach(ServerClient sc in c)
+        {
+            NetworkTransport.Send(hostId, sc.connectionId, channelId, msg, message.Length * sizeof(char), out error);
+        }
+    }
+
+    private void OnNameIs(int cnnId, string playerName)
+    {
+        // Link the name to the connectionId
+        clients.Find(x => x.connectionId == cnnId).playerName = playerName;
+        // Tell everybody that a new player has connected
+        Send("CNN+" + playerName + '|' + cnnId, reliableChannel,clients);
     }
 
 }
