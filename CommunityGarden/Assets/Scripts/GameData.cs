0f1c261ddb738fa5219e2d7eb1194ea8fa1fa996
nicholasbrennanmart <nicholas.brennanmart@mymail.champlain.edu>
2021-04-29 23:11:54 -0400

    started making common game data

---
+++ CommunityGarden/Assets/Scripts/GameData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+
+    GameData[] players;
+    Vector3[] playerPos;
+    Vector3[] playerRotation;
+    Vector3[] playerScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
